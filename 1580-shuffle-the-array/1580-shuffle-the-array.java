0b25f0f5547db651451a592237aa463bc120a734
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-15 15:45:45 +0530

    Time: 0 ms (100%), Space: 43.6 MB (73.36%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int a[]=new int[2*n];
-        int k=0;
-        int j=n,i=0;
+        int k=0,j=n,i=0;
         while(j<2*n)
         {
             a[k++]=nums[i++];
             a[k++]=nums[j++];
         }
-        System.gc();
         return a;
     }
 }
