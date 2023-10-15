ff325ff1d618a1cc25b1f379cf2e275c5f58da6c
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-15 10:59:54 +0530

    Time: 7 ms (6.04%), Space: 41.2 MB (20.77%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -3,12 +3,8 @@ class Solution {
         String s[]=sentence.split("\\s+");
         int n=s.length;
         for(int i=0;i<n;i++)
-        {
-            char a=s[i].charAt(s[i].length()-1);
-            char b=s[(i+1)%n].charAt(0);
-            if(a!=b)
+            if(s[i].charAt(s[i].length()-1)!=s[(i+1)%n].charAt(0))
                 return false;
-        }
         return true;
     }
 }
