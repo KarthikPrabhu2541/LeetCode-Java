915058dd0f614ba86b2f10fe9aff46363c41ba20
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-15 10:58:42 +0530

    Time: 6 ms (7.97%), Space: 41.4 MB (14.98%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -4,10 +4,8 @@ class Solution {
         int n=s.length;
         for(int i=0;i<n;i++)
         {
-            int k=s[i].length();
-            char a=s[i].charAt(k-1);
+            char a=s[i].charAt(s[i].length()-1);
             char b=s[(i+1)%n].charAt(0);
-            System.out.println(a+" "+b);
             if(a!=b)
                 return false;
         }
