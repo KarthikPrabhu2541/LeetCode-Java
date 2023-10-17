e2108709ce07f0d54dafbc9aa6c93adeeda9068d
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-17 21:34:41 +0530

    Time: 3 ms (100%), Space: 84.3 MB (47.62%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,1 +1,1 @@
-class Solution {public int[] rearrangeArray(int[] nums) {int n=nums.length,x=0,y=1;int a[]=new int[n];for(int i=0;i<n;i++){if(nums[i]>0){a[x]=nums[i];x+=2;}else{a[y]=nums[i];y+=2;}}System.gc();return a;}}
+class Solution {public int[] rearrangeArray(int[] nums) {int n=nums.length,x=0,y=1;int a[]=new int[n];for(int i=0;i<n;i++){if(nums[i]>0){a[x]=nums[i];x+=2;}else{a[y]=nums[i];y+=2;}}return a;}}
