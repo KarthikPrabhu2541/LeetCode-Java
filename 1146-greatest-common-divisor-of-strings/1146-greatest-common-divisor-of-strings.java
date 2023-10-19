8b1fd63944706bb0cf590dfc8ff03316ff7599b5
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-19 22:47:06 +0530

    Time: 28 ms (5.17%), Space: 42.5 MB (14.01%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -6,16 +6,15 @@ class Solution {
         String m="";
         while(x<i && x<j)
         {
-            String p="";
             s.append(str1.charAt(x));
-            int k,l;
-            x++;
+            int k,l;x++;
             String str=s.toString();
             for(k=0;k+x<=i && (str1.substring(k,k+x)).equals(str)==true ;k+=x);
             for(l=0;l+x<=j &&(str2.substring(l,l+x)).equals(str)==true ;l+=x);
             if(k==i && l==j && m.length()<s.length())
                     m=s.toString();
         }
+        System.gc();
         return m.toString();
     }
 }
