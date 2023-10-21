0972b707cc34d3a80213615babd7328d22efef7b
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2023-10-21 11:17:03 +0530

    Time: 0 ms (100%), Space: 38.6 MB (95.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,12 @@
 class Solution {
+    int a[]=new int[31];
     public int fib(int n) {
        if(n==0)
         return 0;
        else if(n==1)
         return 1;
-       return fib(n-1)+fib(n-2); 
+       if(a[n]!=0)
+        return a[n];
+       return a[n]=fib(n-1)+fib(n-2); 
     }
 }
