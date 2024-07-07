7e99cbe76c147515d977fd5bb04a29a9212d40f8
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2024-07-07 16:29:46 +0530

    Time: 90 ms (100%), Space: 40 MB (68.63%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int current=numBottles;
         int drinks=numBottles;
         // System.out.println("initial drinks = "+drinks);
-        while(current>=numExchange){
-            // System.out.println("empty = "+current);
-            drinks+=current/numExchange;
-            // System.out.println("New = "+current/numExchange+" empty = "+current%numExchange);
-            current=current/numExchange+current%numExchange;
+        while(numBottles>=numExchange){
+            // System.out.println("empty = "+numBottles);
+            drinks+=numBottles/numExchange;
+            // System.out.println("New = "+numBottles/numExchange+" empty = "+numBottles%numExchange);
+            numBottles=numBottles/numExchange+numBottles%numExchange;
         }
-        // System.out.println("last empty = "+current);
+        // System.out.println("last empty = "+numBottles);
         // System.out.println("Total Drinks = "+drinks);
         return drinks;
     }
