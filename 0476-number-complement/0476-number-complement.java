78b2a38622778e85af7bc3ad4cebd69f4e04a325
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2024-08-22 11:02:51 +0530

    Time: 0 ms (100%), Space: 40.2 MB (67.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,7 +1,10 @@
 class Solution {
     public int findComplement(int num) {
-        int a = (int)(Math.log(num)/Math.log(2));
-        int total = ((int)Math.pow(2,(int)(Math.log(num)/Math.log(2))))*2-1;
-        return (((int)Math.pow(2,(int)(Math.log(num)/Math.log(2))))*2-1)-num;
+        //index value of the most significant bit
+        int mostSigniBitIndex = (int)(Math.log(num)/Math.log(2)); 
+        // maximum value possible for the length of binary number 101 => 7 for length 3
+        int maxValue = ((int)Math.pow(2,mostSigniBitIndex))*2-1; 
+        //the difference of maxvalue and number is the complement of the number
+        return maxValue-num;
     }
 }
