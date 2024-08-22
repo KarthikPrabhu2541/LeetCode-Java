00cdbef4766ddc8c8e928b477fe0a4a038e68878
KARTHIK PRABHU <91746517+KarthikPrabhu2541@users.noreply.github.com>
2024-08-22 10:58:30 +0530

    Time: 0 ms (100%), Space: 40.1 MB (80.96%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findComplement(int num) {
+        int a = (int)(Math.log(num)/Math.log(2));
+        int total = ((int)Math.pow(2,(int)(Math.log(num)/Math.log(2))))*2-1;
         return (((int)Math.pow(2,(int)(Math.log(num)/Math.log(2))))*2-1)-num;
     }
 }
